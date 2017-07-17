ad64a63c0fe8f25a486bdffa6bafa6027d7807eb
Otto Hahn-Herrera <ottovampirito@gmail.com>
2017-07-17 14:24:46 -0500

    Added template handler and 1st template

--- main.go
+++ main.go
@@ -5,21 +5,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(
-			`<html>
-		<head>
-			<title>Chat</title>
-		</head>
-		<body>
-		Let's chat!
-		</body>
-	</html>
-	`))
+// temp1 represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	temp1    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
 	})
+	t.temp1.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
